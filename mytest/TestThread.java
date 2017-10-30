9085817f0c6ce63241a858e2ee0964efd6121a61
chenhs <727926271@qq.com>
2017-10-30 19:24:08 +0800

    合并

--- mytest/TestThread.java
+++ mytest/TestThread.java
@@ -2,8 +2,16 @@ public Class TestThread extends Thread{
 	public run(){
 		sys.print("TestThread");
 	}
-	
+
 	public void testThree(){
 			sys.print("three");
+	}
+
+	private testRun(){
+		int i = 0;
+		if(i==0){
+			sys.print(i);
+		}
+
 	}
 }
