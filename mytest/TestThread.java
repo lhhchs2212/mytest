863e7417f48a2f672f133c1b7702cae865c919c8
chenhs <727926271@qq.com>
2017-10-30 19:14:14 +0800

    testTwo

--- mytest/TestThread.java
+++ mytest/TestThread.java
@@ -2,4 +2,11 @@ public Class TestThread extends Thread{
 	public run(){
 		sys.print("TestThread");
 	}
+	
+	private testRun(){
+		int i = 0;
+		if(i==0){
+			sys.print(i);
+		}
+	}
 }
---
+++ mytest/TestTwo.java
@@ -0,0 +1,26 @@
+/** 
+     * 重置序列号，从1开始 
+     */  
+    protected void resetSerialNum() throws Exception {  
+        this.serialNum = 1;  
+        intervalMax = serialNum + interval;  
+        updateStartNum(intervalMax);  
+    }  
+	
+	
+	 /** 
+     * 是否是最大值 
+     * @param num 
+     * @return 
+     */  
+    private boolean isMax(int num) {  
+        return num >= maxNum;  
+    }  
+      
+    public int getInterval() {  
+        return this.interval;  
+    }  
+      
+    public int getMaxNum() {  
+        return this.maxNum;  
+    }  
